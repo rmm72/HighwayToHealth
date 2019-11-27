a67db7fe6aa8715c0644b14919e636727f5252ff
rmm72 <rmm72@users.noreply.github.com>
2019-11-26 22:56:11 -0500

    Rona Mae's Folder added
    Testing to see if personal folders will be added to the GitHub repo

---
+++ MAIN/Rona Mae/HighwayToHealth/app/src/main/java/com/example/highwaytohealth/CalorieCalculator.java
@@ -0,0 +1,14 @@
+package com.example.highwaytohealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalorieCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_calculator);
+    }
+}
